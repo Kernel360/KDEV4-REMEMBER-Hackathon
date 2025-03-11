a4e1eaf702c7e5b3f461ecbfe5f29a69ba18fcb1
codingjjh0515 <greenjjh0515@naver.com>
2025-03-11 15:23:39 +0900

    PostRequestDto 작업 전

--- src/main/java/dev/laterre/nyamnyam/member/dto/MemberUpdateDto.java
+++ src/main/java/dev/laterre/nyamnyam/member/dto/MemberUpdateDto.java
@@ -1,10 +1,13 @@
 package dev.laterre.nyamnyam.member.dto;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class MemberUpdateDto {
 
     private String email;
--- src/main/java/dev/laterre/nyamnyam/post/dto/PostUpdateDto.java
+++ src/main/java/dev/laterre/nyamnyam/post/dto/PostUpdateDto.java
@@ -1,10 +1,13 @@
 package dev.laterre.nyamnyam.post.dto;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class PostUpdateDto {
 
     private String title;
