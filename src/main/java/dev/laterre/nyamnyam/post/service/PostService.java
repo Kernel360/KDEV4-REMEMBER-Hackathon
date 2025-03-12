9ee5fec0e7fdc610f367fd93739d4cef25d2f0ad
Cheon SeungJun <55969783+jun950829@users.noreply.github.com>
2025-03-12 11:21:15 +0900

    getPostsByBoard 수정

--- src/main/java/dev/laterre/nyamnyam/post/service/PostService.java
+++ src/main/java/dev/laterre/nyamnyam/post/service/PostService.java
@@ -12,6 +12,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
@@ -180,7 +181,7 @@ public class PostService {
 
     @Transactional(readOnly = true)
     public Page<PostEntity> getPostsByBoard(Long boardId, int page, int size) {
-        Pageable pageable = PageRequest.of(page, size);
+        Pageable pageable = PageRequest.of(page, size, Sort.by("id").descending());;
         return postRepository.findByBoardId(boardId, pageable);
     }
 
